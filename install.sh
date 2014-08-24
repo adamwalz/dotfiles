b0b9ce26c5e0659e0807e0c77c8316aede5308ae
Adam Walz <adam@adamwalz.net>
2014-08-23 23:24:10 -0700

    Update installer to my latest preferences

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 #          FILE:  install.sh
 #   DESCRIPTION:  Script to clone and install all dotfiles from github
 #        AUTHOR:  Adam Walz <adam@adamwalz.net>
-#       VERSION:  1.0.0
+#       VERSION:  1.0.1
 #------------------------------------------------------------------------------
 
-DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_DIR="$HOME/Developer/setup/dotfiles"
 PREZTO_DIR="${ZDOTDIR:-$HOME}/.zprezto"
 SSH_DIR="$HOME/.ssh"
 
 if [ ! -d "$DOTFILES_DIR" ]; then
-    git clone https://github.com/adamwalz/dotfiles.git $DOTFILES_DIR
+    git clone git@github.com:adamwalz/dotfiles.git $DOTFILES_DIR
 fi
 
 if [ ! -d "$PREZTO_DIR" ]; then
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git $PREZTO_DIR
+    git clone --recursive git@github.com:adamwalz/prezto.git $PREZTO_DIR
 fi
 
 if [ ! -d "$SSH_DIR" ]; then
@@ -26,4 +26,4 @@ if [[ "$SHELL" != *zsh ]]; then
     chsh -s /bin/zsh
 fi
 
-rake -f $DOTFILES_DIR/Rakefile
+rake install -f $DOTFILES_DIR/Rakefile
