894bfe8816b72e05a34411aba8f866fa3630c9fc
Adam Walz <adam@adamwalz.net>
2016-06-14 14:52:38 -0600

    Add function to get homebrew installed leaves
    Leaves are formula without a dependent parent

---
+++ tilde/bin/brew-leaves.rb
@@ -0,0 +1,7 @@
+require 'formula'
+deps_graph = Formula.get_used_by
+formulas = HOMEBREW_CELLAR.children.select { |pn| pn.directory? }.collect { |pn| pn.basename.to_s }
+formulas.each do |name|
+  deps = deps_graph[name] || []
+  puts name if !deps.any? { |dep| formulas.include?(dep) }
+end
