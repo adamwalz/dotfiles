6900176d3b9d904ef3e4d6d607eaeaea2ccc5888
Adam Walz <adam@adamwalz.net>
2017-10-16 11:25:11 -0700

    Add bitbar OS X menu bar configs

---
+++ config/bitbar/unixtime.5s.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# <bitbar.title>Unix Time</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Mat Ryer</bitbar.author>
+# <bitbar.author.github>matryer</bitbar.author.github>
+# <bitbar.desc>Displays unix time.</bitbar.desc>
+# <bitbar.image>http://i.imgur.com/h2cyuYu.png</bitbar.image>
+
+date +%s
